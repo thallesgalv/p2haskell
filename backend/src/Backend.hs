1ec7e936483636dec41db0004a7fc52064d852b6
Thalles Galvao <thallesgalv@gmail.com>
2022-06-14 19:58:50 -0300

    Backend sessao 4.3

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -6,14 +6,16 @@ import Obelisk.Backend
 import Database.PostgreSQL.Simple
 
 getConn :: ConnectInfo
-getConn = ConnectInfo "maquina"
+getConn = ConnectInfo "ec2-52-86-56-90.compute-1.amazonaws.com"
                       5432 -- porta
-                      "usuario"
-                      "senha"
-                      "banco"
+                      "nekyxozewcrclx"
+                      "e0464e50ceac30efd6c8e79293ea27808cfca9f70679eeaf09bff2279dd8c5ea"
+                      "d5du3q4bgkidfd"
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve -> do
+    dbcon <- connect getConn
+    serve $ const $ return ()
   , _backend_routeEncoder = fullRouteEncoder
   }
